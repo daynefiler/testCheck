a4ba5b94b9db64fb9d21dffdd4eaa04768d2ce3c
daynefiler <dayne.filer@gmail.com>
2022-01-28 11:09:25 -0500

    Load package

---
+++ R/buildNamespace.R
@@ -0,0 +1,16 @@
+#' @name buildNamespace
+#' @title Build NAMESPACE
+#' @description Build NAMESPACE
+#' @importClassesFrom S4Vectors List SimpleList character_OR_NULL
+#' @importFrom BiocGenerics as.data.frame duplicated sort updateObject
+#' @importFrom S4Vectors List classNameForDisplay coolcat new2 setValidity2
+#' @importFrom grDevices colorRampPalette
+#' @importFrom pryr substitute_q make_function
+#' @importFrom rlang is_scalar_character is_scalar_integerish is_scalar_logical
+#' @importFrom stats sd
+#' @importFrom tidyr separate
+#' @importFrom utils data
+#' @importFrom utils read.csv
+#' @importFrom utils type.convert
+
+NULL
