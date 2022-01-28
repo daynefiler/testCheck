53a57c5296be1e239dc8236466c78d37edbc5965
daynefiler <dayne.filer@gmail.com>
2022-01-28 11:22:46 -0500

    Added grid, methods imports

--- R/buildNamespace.R
+++ R/buildNamespace.R
@@ -12,5 +12,50 @@
 #' @importFrom utils data
 #' @importFrom utils read.csv
 #' @importFrom utils type.convert
+#' @import grid
+#' @import methods
 
 NULL
+
+#
+# + BiocGenerics   0.40.0     [bld][dl]
+# + callr          3.7.0      [bld][dl]
+# + cli            3.1.1      [bld][cmp][dl]
+# + cpp11          0.4.2      [bld][dl]
+# + crayon         1.4.2      [bld][dl]
+# + curl           4.3.2      [bld][cmp][dl]
+# + desc           1.4.0      [bld][dl]
+# + digest         0.6.29     [bld][cmp][dl]
+# + dplyr          1.0.7      [bld][cmp][dl]
+# + ellipsis       0.3.2      [bld][cmp][dl]
+# + fansi          1.0.2      [bld][cmp][dl]
+# + generics       0.1.1      [bld][dl]
+# + glue           1.6.1      [bld][cmp][dl]
+# + lifecycle      1.0.1      [bld][dl]
+# + lobstr         1.1.1      [bld][cmp][dl]
+# + magrittr       2.0.2      [bld][cmp][dl]
+# + pillar         1.6.5      [bld][dl]
+# + pkgbuild       1.3.1      [bld][dl]
+# + pkgconfig      2.0.3      [bld][dl]
+# + prettyunits    1.1.1      [bld][dl]
+# + processx       3.5.2      [bld][cmp][dl]
+# + pryr           0.1.5      [bld][cmp][dl]
+# + ps             1.6.0      [bld][cmp][dl]
+# + purrr          0.3.4      [bld][cmp][dl]
+# + R6             2.5.1      [bld][dl]
+# + rcmdcheck      1.4.0      [bld][dl]
+# + Rcpp           1.0.8      [bld][cmp][dl]
+# + rlang          1.0.0      [bld][cmp][dl]
+# + rprojroot      2.0.2      [bld][dl]
+# + S4Vectors      0.32.3     [bld][cmp][dl]
+# + sessioninfo    1.2.2      [bld][dl]
+# + stringi        1.7.6      [bld][cmp][dl]
+# + stringr        1.4.0      [bld][dl]
+# + testCheck      0.0.0.9000 [bld][dl]
+# + tibble         3.1.6      [bld][cmp][dl]
+# + tidyr          1.1.4      [bld][cmp][dl]
+# + tidyselect     1.1.1      [bld][cmp][dl]
+# + utf8           1.2.2      [bld][cmp][dl]
+# + vctrs          0.3.8      [bld][cmp][dl]
+# + withr          2.4.3      [bld][dl]
+# + xopen          1.0.0      [bld][dl]
